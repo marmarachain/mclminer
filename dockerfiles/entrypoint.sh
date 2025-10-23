e1ce1068ba5ab8534a6e3bdc524d652d5ecee528
aklix <abdlkdrylmz@gmail.com>
2025-10-23 18:19:16 +0300

    dockerfile changed . config added

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -6,7 +6,6 @@ MCL_CONF="${MCL_DIR}/MCL.conf"
 STRATUM_PATH="/home/mclminer/yiimp-stratum-equihash"
 DB_CPP="${STRATUM_PATH}/db.cpp"
 
-chown -R mclminer:usergroup "$MCL_DIR"
 if [ ! -d "$MCL_DIR" ]; then
   echo "==> Creating directory $MCL_DIR ..."
   mkdir -p "$MCL_DIR"
@@ -29,18 +28,6 @@ else
   sed -i "s/^rpcuser=.*/rpcuser=${RPC_USER}/" "$MCL_CONF"
   sed -i "s/^rpcpassword=.*/rpcpassword=${RPC_PASS}/" "$MCL_CONF"
 fi
-
-
-echo "==> Updating db.cpp with environment variables ..."
-
-# rpc.host satırı (// [2] rpc.host)
-if [ -n "$RPC_HOST" ]; then
-  sed -i "s|.*// \[2\] rpc.host.*|            \"${RPC_HOST}\", // [2] rpc.host|" "$DB_CPP"
-fi
-sed -i "s|.*// \[4\] rpc.user.*|            \"${RPC_USER}\", // [4] rpc.user|" "$DB_CPP"
-sed -i "s|.*// \[5\] rpc.pass.*|            \"${RPC_PASS}\", // [5] rpc.pass|" "$DB_CPP"
-sed -i "s|.*// \[7\] wallet.*|            \"${WALLET}\", // [7] wallet|" "$DB_CPP"
-
 
 # Start fetch-params.sh
 /home/mclminer/fetch-params.sh
